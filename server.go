fe5accce11a876f8f458ea6750ba1e2349adc298
willdebras <wtb3na@virginia.edu>
2021-04-13 15:49:39 -0500

    added functtions tto load, save, encode data

--- server.go
+++ server.go
@@ -3,14 +3,19 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi"
 )
+
+var StoragePath = "/tmp"
 
 func main() {
 	// Get port from env variables or set to 8080.
@@ -83,4 +88,125 @@ func JSON(w http.ResponseWriter, data interface{}) {
 	}
 
 	w.Write(b)
+}
+
+func Get(ctx context.Context, key string) (string, error) {
+	data, err := loadData(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return data[key], nil
+}
+
+func Set(ctx context.Context, key string, value string) error {
+	data, err := loadData(ctx)
+	if err != nil {
+		return err
+	}
+
+	data[key] = value
+	if err := saveData(ctx, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Delete(ctx context.Context, key string) error {
+	data, err := loadData(ctx)
+	if err != nil {
+		return err
+	}
+
+	delete(data, key)
+	return nil
+}
+
+func dataPath() string {
+	return filepath.Join(StoragePath, "data.json")
+}
+
+func loadData(ctx context.Context) (map[string]string, error) {
+	empty := map[string]string{}
+	emptyData, err := encode(map[string]string{})
+	if err != nil {
+		return empty, err
+	}
+
+	// First check if the folder exists and create it if it is missing.
+	if _, err := os.Stat(StoragePath); os.IsNotExist(err) {
+		err = os.MkdirAll(StoragePath, 0755)
+		if err != nil {
+			return empty, err
+		}
+	}
+
+	// Then check if the file exists and create it if it is missing.
+	if _, err := os.Stat(dataPath()); os.IsNotExist(err) {
+		err := os.WriteFile(dataPath(), emptyData, 0644)
+		if err != nil {
+			return empty, err
+		}
+	}
+
+	content, err := os.ReadFile(dataPath())
+	if err != nil {
+		return empty, err
+	}
+
+	return decode(content)
+}
+
+func saveData(ctx context.Context, data map[string]string) error {
+	// First check if the folder exists and create it if it is missing.
+	if _, err := os.Stat(StoragePath); os.IsNotExist(err) {
+		err = os.MkdirAll(StoragePath, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	encodedData, err := encode(data)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(dataPath(), encodedData, 0644)
+}
+
+func encode(data map[string]string) ([]byte, error) {
+	encodedData := map[string]string{}
+	for k, v := range data {
+		ek := base64.URLEncoding.EncodeToString([]byte(k))
+		ev := base64.URLEncoding.EncodeToString([]byte(v))
+		encodedData[ek] = ev
+	}
+
+	return json.Marshal(encodedData)
+}
+
+func decode(data []byte) (map[string]string, error) {
+	var jsonData map[string]string
+
+	if err := json.Unmarshal(data, &jsonData); err != nil {
+		return nil, err
+	}
+
+	returnData := map[string]string{}
+	for k, v := range jsonData {
+		dk, err := base64.URLEncoding.DecodeString(k)
+		if err != nil {
+			return nil, err
+		}
+
+		dv, err := base64.URLEncoding.DecodeString(v)
+		if err != nil {
+			return nil, err
+		}
+
+		returnData[string(dk)] = string(dv)
+	}
+
+	return returnData, nil
 }
